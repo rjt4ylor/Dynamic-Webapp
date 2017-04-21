2af3d0f895afbf6ccad1b23112caf991255deddd
CalumAlexanderStewart <Calumalexanderstewart@gmail.com>
2017-04-21 18:18:05 +0100

    Update app2.js

--- app2.js
+++ app2.js
@@ -41,7 +41,7 @@ function onSearchResponse(response) {
 	
 	var results1 = result1.replace(/['"]+/g, '');
 	
-	showResponse('<iframe width="854" height="480" src="https://www.youtube.com/embed/-qpiVgg3Y4c?ecver=1" frameborder="0" allowfullscreen></iframe>');
+	document.write('<iframe width="854" height="480" src="https://www.youtube.com/embed/-qpiVgg3Y4c?ecver=1" frameborder="0" allowfullscreen></iframe>');
 	
     
 	showResponse("<li>" +result2+"ddd</li>");
